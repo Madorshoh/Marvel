985acf865a42a90e4548384acda05f5df581416b
Madorshoh <madorchikturgunboyev1993@gmail.com>
2023-06-12 15:36:36 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/header/header'
 import Slider from './components/slider/slider'
 import Card from './components/card/card'
 import Guardians from './components/Guardians of the Galaxy Vol. 2/guardians_of_the_galaxy_vol_2'
+import './App.css'
 
 
 function App() {
